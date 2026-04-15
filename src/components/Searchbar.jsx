b3cc0a6e53f79d226cad93b23a7a64e6b96684c4
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2026-04-15 23:31:32 +0530

    Debugging

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -12,6 +12,7 @@ const Searchbar = () => {
             strokeWidth="2"
             viewBox="0 0 24 24"
           >
+            console.log("Debugging");
             <circle cx="11" cy="11" r="8"></circle>
             <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
           </svg>
