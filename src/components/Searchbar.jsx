9177eafd3a717c2eabbc019a725d5a2aeff303af
Vinit Kumar Singh <VinitSingh.dev@gmail.com>
2026-06-01 23:50:24 +0530

    Debugging

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -25,6 +25,9 @@ const Searchbar = () => {
         <button className="bg-purple-800 rounded-md text-white font-semibold px-10 py-2 active:scale-95">
           Search
         </button>
+        <button className="bg-purple-800 rounded-md text-white font-semibold px-10 py-2 active:scale-95">
+          Search
+        </button>
       </form>
     </div>
   );
